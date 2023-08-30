afa6d5da3c5a2d4c468110f25931c8eb9476341d
Станислав Зиганшин <sziganshin@web-bee.ru>
2023-08-31 00:21:33 +0300

    feat--add-swagger

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Mebel V Dom API')
+    .setDescription('Description')
+    .setVersion('1.0')
+    .addTag('mvd')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   app.setGlobalPrefix('v1');
   app.enableCors();
   await app.listen(8001);
