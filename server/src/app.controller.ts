21b93cab9a7e6e2e2326183eaf7ac95bef2468f5
M P <m@steio.ru>
2023-08-31 19:22:47 +0300

    refactor: add comments & optimize code

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -1,11 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, HttpStatus, HttpCode } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
+  // Внедрение зависимости AppService через конструктор.
   constructor(private readonly appService: AppService) {}
 
+  // Обработка HTTP GET запросов на корневой маршрут.
   @Get()
+  @HttpCode(HttpStatus.OK)
   getHello(): string {
     return this.appService.getHello();
   }
--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -7,20 +7,31 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
+    // Импорт других модулей приложения
     AdminsModule,
+
+    // Инициализация глобального модуля конфигурации
     ConfigModule.forRoot({ isGlobal: true }),
+
+    // Асинхронная инициализация TypeORM с использованием конфигурационного сервиса
     TypeOrmModule.forRootAsync({
+
+      // Явный импорт ConfigModule для доступа к переменным окружения
       imports: [ConfigModule],
+
+      // Асинхронная конфигурация TypeORM
       useFactory: (configService: ConfigService) => ({
-        type: 'postgres',
-        host: configService.get('DB_HOST'),
-        port: configService.get('DB_PORT'),
-        username: configService.get('DB_USERNAME'),
-        password: configService.get('DB_PASSWORD'),
-        database: configService.get('DB_NAME'),
-        synchronize: true,
-        entities: [__dirname + '/**/*.entity{.js, .ts}'],
+        type: 'postgres', // тип базы данных
+        host: configService.get('DB_HOST'), // хост базы данных
+        port: configService.get('DB_PORT'), // порт для подключения
+        username: configService.get('DB_USERNAME'), // имя пользователя
+        password: configService.get('DB_PASSWORD'), // пароль пользователя
+        database: configService.get('DB_NAME'), // имя базы данных
+        synchronize: true, // автоматическая синхронизация схемы (только на этапе разработки!)
+        entities: [__dirname + '/**/*.entity{.js, .ts}'], // пути к сущностям
       }),
+
+      // Внедрение зависимости ConfigService для доступа к переменным окружения
       inject: [ConfigService],
     }),
   ],
