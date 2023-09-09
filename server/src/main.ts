e637ac5fd995f0b454419265d929ed930e30de63
Станислав Зиганшин <sziganshin@web-bee.ru>
2023-09-09 12:44:37 +0300

    fix-maints

--- server/src/main.ts
+++ server/src/main.ts
@@ -6,8 +6,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: '*',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    credentials: true,
   });
   const config = new DocumentBuilder()
     .setTitle('Mebel V Dom API')
